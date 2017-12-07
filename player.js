78fa50268f21eb49994937747c7fc7af218ac2d1
BaselNsralla <basel.obeid9661@gmail.com>
2017-12-07 21:33:13 +0100

    more refactoring

--- player.js
+++ player.js
@@ -1,11 +1,14 @@
+const allConnections = []
 const joinChannel = (voiceChannel, initialSong) => {
     voiceChannel.join().then(connection => {
-        var pl = new Playlist(message.channel.id, init);
-        connection.playlist = pl;
-        init++
+        const uid = Date.now()
+        var playlist = new Playlist(message.channel.id, uid);
+        connection.playlist = playlist;
+        connection.id = uid
+        allConnections.push(connection)
         //ytdl ger mig en readable stream från en länk med vid,music
         //en gång per join som beyder att jag kan s
         console.log("Play")
-        search(firstSong,connection,pl,"first",message)
+        search(firstSong,connection, playlist, 'first', message)
     }).catch(console.error)
 }
--- playlist.js
+++ playlist.js
@@ -1,24 +1,18 @@
 
 
 const eventEmitter = require("events");
-function playlist(id,uid){
+function playlist(id, uid){
     this.list = [];
     this.skip = function(){
         if (list.length!=0){
-      this.list.shift();      
-
+            this.list.shift();
         }
-
     };
-
     this.id = id;
     this.add= function(s){
         this.list.push(s);
-
-
     };
    this.uid = uid;
-
 }
 
 
